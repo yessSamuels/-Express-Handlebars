5e8db11cb339bd0e8afd622fd8dddb7ae4448229
Yessica Samuels <yessicaperez@me.com>
2021-02-04 17:13:01 -0800

    new

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+DROP DATABASE burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, eaten) VALUES ('Viggie Burger', false);
+INSERT INTO burgers (name, eaten ) VALUES ('Shu Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Apple Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Makers Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Everything Burger', true);
